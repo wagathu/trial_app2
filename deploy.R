bdacc608a800abbe8dd0e155301260c7d99c9ae4
wagathu <briannjuguna133@gmail.com>
2025-03-03 22:33:30 +0300

    My second trial

--- deploy.R
+++ deploy.R
@@ -1,7 +1,7 @@
 # Authenticate
 rsconnect::setAccountInfo(name = "wagathu",
-               token = "EED42A134EDEDCE0E74B0297DBB5D1E9",
-               secret = "lEprluutegILA9RlMAVnfRA3+xAd/O3PNkkxyE+1"
-               )
-# Deploy
+                          token = "04B78DEBBED7CA750FE1C4410ADDB92E",
+                          secret = "6W+Dln3QZqEJ3XW2AGjcmu0aw9Pnea3rTdQqKusF"
+)
+
 rsconnect::deployApp(appFiles = c("app.R", "data"))
