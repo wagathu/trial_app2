22d76358cf17945e350131bf32d0e773d62c05bd
wagathu <briannjuguna133@gmail.com>
2025-03-04 00:17:13 +0300

    My 7th automatic trial

--- app.R
+++ app.R
@@ -1,6 +1,5 @@
 
 source("global.R")
-googlesheets4::gs4_auth(email = "brian.njuguna@cema.africa", cache = ".secrets")
 ui <- fluidPage(
     fluidRow(
         h2("Trial app", style = "color:#27AAE1; text-align:center"),
