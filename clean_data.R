7a7e05662066ad6176eb3b04ad1c461b65f576c4
wagathu <briannjuguna133@gmail.com>
2025-03-04 19:25:41 +0300

    my 22nd automatic trial

--- clean_data.R
+++ clean_data.R
@@ -10,8 +10,10 @@ pacman::p_load(
 
 # Importing packages ------------------------------------------------------
 
-
-gs4_auth_configure(api_key = "9c1c7493a4bfa59827d042410d7956a64591bf56")
+gs4_auth(
+  cache = ".secrets",
+  email = "briannjuguna133@gmail.com"
+)
 url <- "https://docs.google.com/spreadsheets/d/1rqkgsSAXkAy3eUfL5HKaDRP7VNa_5sBSgf2W_H6gdzY/edit?gid=0#gid=0"
 df <- read_sheet(url)
 fwrite(df, "data/dd.csv")
