6ae6f2657db099e90dd81bc5edff5887440e398f
wagathu <briannjuguna133@gmail.com>
2025-03-04 21:31:22 +0300

    my 28th automatic trial

--- clean_data.R
+++ clean_data.R
@@ -11,12 +11,8 @@ pacman::p_load(
 # Importing packages ------------------------------------------------------
 
 # google-drive
-t <- gargle::credentials_service_account(jsonlite::fromJSON(Sys.getenv("GOOGLE_SHEETS_KEY")))
-googledrive::drive_auth(token = t)
-googlesheets4::gs4_auth(token = t)
+gs4_auth(email = "briannjuguna133@gmail.com", cache = ".secrets")
+
 url <- "https://docs.google.com/spreadsheets/d/1rqkgsSAXkAy3eUfL5HKaDRP7VNa_5sBSgf2W_H6gdzY/edit?gid=0#gid=0"
 df <- read_sheet(url)
 fwrite(df, "data/dd.csv")
-
-
-readxl::read_excel(path = "https://docs.google.com/spreadsheets/d/1rqkgsSAXkAy3eUfL5HKaDRP7VNa_5sBSgf2W_H6gdzY/edit?usp=sharing")
